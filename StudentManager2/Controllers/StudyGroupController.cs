764f05c99b4cc7d153f4a6e1267df0d6fa65cad5
Yoni-Satat <yoni.satat@gmail.com>
2018-04-21 17:17:40 +0100

    scaffolded study group controller and vew

---
+++ StudentManager2/Controllers/StudyGroupController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentManager2.DAL;
+using StudentManager2.Models;
+
+namespace StudentManager2.Controllers
+{
+    public class StudyGroupController : Controller
+    {
+        private StudentContext db = new StudentContext();
+
+        // GET: StudyGroup
+        public ActionResult Index()
+        {
+            var studyGroups = db.StudyGroups.Include(s => s.Course);
+            return View(studyGroups.ToList());
+        }
+
+        // GET: StudyGroup/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudyGroup studyGroup = db.StudyGroups.Find(id);
+            if (studyGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studyGroup);
+        }
+
+        // GET: StudyGroup/Create
+        public ActionResult Create()
+        {
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title");
+            return View();
+        }
+
+        // POST: StudyGroup/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudyGroupID,CourseID,GroupTitle")] StudyGroup studyGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StudyGroups.Add(studyGroup);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", studyGroup.CourseID);
+            return View(studyGroup);
+        }
+
+        // GET: StudyGroup/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudyGroup studyGroup = db.StudyGroups.Find(id);
+            if (studyGroup == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", studyGroup.CourseID);
+            return View(studyGroup);
+        }
+
+        // POST: StudyGroup/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StudyGroupID,CourseID,GroupTitle")] StudyGroup studyGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studyGroup).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", studyGroup.CourseID);
+            return View(studyGroup);
+        }
+
+        // GET: StudyGroup/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudyGroup studyGroup = db.StudyGroups.Find(id);
+            if (studyGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studyGroup);
+        }
+
+        // POST: StudyGroup/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StudyGroup studyGroup = db.StudyGroups.Find(id);
+            db.StudyGroups.Remove(studyGroup);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
